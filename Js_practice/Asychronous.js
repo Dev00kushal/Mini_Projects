c73d6749ba22ff5f8bd65a8b2e2dc881ca5eb6af
Devkushal <devkushal00@gmail.com>
2023-05-09 03:34:17 +0545

    Added new jokes

--- Js_practice/Asychronous.js
+++
@@ -1,8 +0,0 @@
-// Asychronous code is the code which does not traffic  the call stack and let the code execute
-// While the call stack is not empty the code waits on Callback Queue 
-
-setTimeout(function my_fn(){
-    console.log("I am the first")
-}, 5000);
-console.log("I am the second ");
-console.log("I am the last ");
--- Js_practice/number.js
+++
@@ -1,9 +0,0 @@
-// There are other methods too in javascript. I have kept here only few of them 
-
-// tofixed method (removes the unecessary numbers in the back )
-let pi_value = 3.141592653589793238;
-console.log(pi_value.toFixed(3));
-
-// To string 
-let number_of_game_played = 555;
-console.log(number_of_game_played.toString());
