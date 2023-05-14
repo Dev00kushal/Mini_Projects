1be74c19721d67ea8ed225253d4ed9e69b93edfe
Devkushal <devkushal00@gmail.com>
2023-05-14 21:18:52 +0545

    Basic implementation

---
+++ Quiz App/index.js
@@ -0,0 +1,27 @@
+let answer_one = document.getElementById("ans-1");
+let answer_two = document.getElementById("ans-2");
+let answer_three = document.getElementById("ans-3");
+
+answer_one.addEventListener("click",()=>{
+    setTimeout(()=>{
+        answer_one.style.backgroundColor = "green";
+        answer_two.style.backgroundColor = "red";
+        answer_three.style.backgroundColor = "red";
+    },1000)
+ })
+ answer_two.addEventListener("click",()=>{
+    setTimeout(()=>{
+        answer_one.style.backgroundColor = "green";
+        answer_two.style.backgroundColor = "red";
+        answer_three.style.backgroundColor = "red";
+    },1000)
+ })
+ answer_three.addEventListener("click",()=>{
+    setTimeout(()=>{
+        answer_one.style.backgroundColor = "green";
+        answer_two.style.backgroundColor = "red";
+        answer_three.style.backgroundColor = "red";
+    },1000)
+ })
+
+
