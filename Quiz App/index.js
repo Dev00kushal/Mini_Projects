20cffde5885a89d56615971858cb284fec570a04
Devkushal <devkushal00@gmail.com>
2023-05-15 10:38:59 +0545

    stupid_app

--- Quiz App/index.js
+++ Quiz App/index.js
@@ -1,13 +1,15 @@
 let answer_one = document.getElementById("ans-1");
 let answer_two = document.getElementById("ans-2");
 let answer_three = document.getElementById("ans-3");
+let popup = document.querySelector(".popup")    
 
 answer_one.addEventListener("click",()=>{
     setTimeout(()=>{
         answer_one.style.backgroundColor = "green";
         answer_two.style.backgroundColor = "red";
         answer_three.style.backgroundColor = "red";
-    },1000)
+        // popup.style.display = "block";
+    },1000) 
  })
  answer_two.addEventListener("click",()=>{
     setTimeout(()=>{
@@ -23,5 +25,6 @@ answer_one.addEventListener("click",()=>{
         answer_three.style.backgroundColor = "red";
     },1000)
  })
+
 
 
