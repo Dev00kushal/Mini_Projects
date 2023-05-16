4aca4f5b5677af263b4fa9faa52769f464deea2a
Devkushal <devkushal00@gmail.com>
2023-05-17 03:26:35 +0545

    Add : more options

--- Quiz App/index.js
+++ Quiz App/index.js
@@ -2,15 +2,30 @@ const question = document.getElementById("question")
 let  option_1 = document.getElementById("opt-1")
 let  option_2 = document.getElementById("opt-2")
 let  option_3 = document.getElementById("opt-3")
-const url = "https://quizapi.io/api/v1/questions?apiKey=n7kKFxRjleTJgNC0lPCthBDq63VkHmijgCsGwc3J&category=linux&difficulty=Medium&limit=10&tags=Linux"
+let  option_4 = document.getElementById("opt-4")
+let  option_5 = document.getElementById("opt-5")
+let  option_6 = document.getElementById("opt-6")
+const url = "https://quizapi.io/api/v1/questions?apiKey=FylkAmzCcCtZZds4y0raA7gkD6tkQR2BYOcvTQNG&category=linux&difficulty=Easy&limit=10&tags=Linux"
 
-const getQuiz = () =>{
-  
-    for (let i = 0; i < 10; i++) {
-        fetch(url)
-        .then((data)=> data.json())
-        .then((data)=> console.log(data[i].question))
+const getQuiz = () =>{  
+    fetch(url)
+    .then((data)=> data.json())
+    .then((data)=> {
+            for (let i = 0; i < 10; i++) {
+            let ques = data[i].question
+            let ans =  data[i].answers   
+            console.log(data)
+            console.log(data[i].correct_answer)
+            question.innerHTML = "Q. " + ques;
+            option_1.innerHTML = "a) " + ans.answer_a ;
+            option_2.innerHTML = "b) " + ans.answer_b ;
+            option_3.innerHTML = "c) " + ans.answer_c ;
+            option_4.innerHTML = "c) " + ans.answer_d ;
+            option_5.innerHTML = "c) " + ans.answer_e ;
+            option_6.innerHTML = "c) " + ans.answer_f ;
+            break
+            }
+    })
         .catch((error)=> console.log(error))
     } 
-}
 getQuiz()
